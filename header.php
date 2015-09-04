1b75531a21d7199f464b1d33013a64ea90321a88
Toast Design <info@toastdesign.nl>
2015-09-04 15:40:00 +0200

    removed description from header

--- header.php
+++ header.php
@@ -26,10 +26,7 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
-
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-
-			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
